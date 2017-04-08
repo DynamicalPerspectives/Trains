625468ccafd79bf6fb6204350cce26ece5e22750
Jeanmarie <jeanmarie@dynamicalperspectives.com>
2017-04-08 13:17:20 -0400

    more changes to styling and notes

---
+++ assets/javascript/trains.js
@@ -0,0 +1,60 @@
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+$("#add").on("click", function() {
+    var Name = $('#Name').val().trim();
+    var Destination = $('#destination').val().trim();
+    var First = moment($('#First').val().trim(), "HH:mm").format("X");
+    var Frequency = $('#frequency').val().trim();
+    if (Name != "" && Destination != "" && First != "" && Frequency != "") {
+        database.ref().push({
+            name: Name,
+            dest: Destination,
+            first: First,
+            freq: Frequency,
+        });
+        alert("Train successfully added!");
+    } else {
+        alert("Please complete each missing field.");
+    };
+    $('#Name').val("");
+    $('#destination').val("");
+    $('#First').val("");
+    $('#frequency').val("");
+
+    return false;
+});
+
+database.ref().on("child_added", function(snapshot) {
+
+    var Name = snapshot.val().name;
+    var Destination = snapshot.val().dest;
+    var trainFirst = snapshot.val().first;
+    var trainFrequency = snapshot.val().freq;
+    var currentTime = moment();
+    var currentTimeConverted = moment(currentTime).format("X");
+    var firstTimePushed = moment(trainFirst, "X").subtract(1, "days");
+    var diffTime = moment(currentTimeConverted, "X").diff(moment(firstTimePushed, "X"), "minutes");
+    var trainRem = diffTime % trainFrequency;
+    var trainWait = trainFrequency - trainRem;
+    var nextTrain = moment().add(trainWait, "minutes");
+    if (currentTimeConverted > trainFirst) {
+        $("#table > tbody").append("<tr><td>" + Name + "</td><td>" +
+            Destination + "</td><td>" +
+            trainFrequency + "</td><td>" +
+            moment(nextTrain).format("hh:mm A") + "</td><td>" +
+            trainWait + "</td></tr>");
+
+    } else {
+
+        $("#table > tbody").append("<tr><td>" + Name + "</td><td>" +
+            Destination + "</td><td>" +
+            trainFrequency + "</td><td>" +
+            moment(trainFirst, "X").format("hh:mm A") + "</td><td>" +
+            trainWait + "</td></tr>");
+    }
+}, function(errorObject) {
+
+    console.log("Errors handled: " + errorObject.code)
+})
